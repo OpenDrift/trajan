d77c2a7909f663b63686a0aa848b2b8a4ff28c54
knutfrode <knutfrode@gmail.com>
2022-11-14 17:11:33 +0100

    Added hack to show OpenDrift output files with trajanshow

--- trajan/scripts/trajanshow.py
+++ trajan/scripts/trajanshow.py
@@ -7,6 +7,8 @@ import trajan as ta
 def main():
     tf = sys.argv[1]
     ds = xr.open_dataset(tf)
+    if 'status' in ds:  # hack for OpenDrift files
+        ds = ds.where(ds.status>=0)
     ds.traj.plot()
 
 if __name__ == '__main__':
