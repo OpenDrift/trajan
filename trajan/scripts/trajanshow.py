601e5d323e16307e047139897728948fd03475b6
Gaute Hope <gaute.hope@met.no>
2022-11-23 14:06:56 +0100

    ts: use .traj syntax

--- trajan/scripts/trajanshow.py
+++ trajan/scripts/trajanshow.py
@@ -12,11 +12,15 @@ def main(tf):
     ds = xr.open_dataset(tf)
     if 'status' in ds:  # hack for OpenDrift files
         ds = ds.where(ds.status>=0)
-    ax, fig, gcrs = ta.plot(ds, show=False)
+
+    ds.traj.plot()
+
     start_time = np.nanmin(ds.time.data).astype('datetime64[s]')
     end_time = np.nanmax(ds.time.data).astype('datetime64[s]')
     name = tf
-    ax.set_title(f'{name} [ {start_time} to {end_time} ]')
+
+    ds.traj.plot.axes.set_title(f'{name} [ {start_time} to {end_time} ]')
+
     plt.show()
 
 if __name__ == '__main__':
