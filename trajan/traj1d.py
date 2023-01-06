e811f2794ca2bc3642776ec5d5dbb41d9dc3af2a
knutfrode <knutfrode@gmail.com>
2023-01-06 11:39:17 +0100

    Made new methods to calculate drifter velocity components, and distance between positions. Also new method to calculate velocity spectrum, with example

--- trajan/traj1d.py
+++ trajan/traj1d.py
@@ -1,4 +1,8 @@
+import numpy as np
+import scipy as sp
+import xarray as xr
 from .traj import Traj
+
 
 class Traj1d(Traj):
     """
@@ -12,3 +16,27 @@ class Traj1d(Traj):
     def time_to_next(self):
         time_step = self.ds.time[1] - self.ds.time[0]
         return time_step
+
+    def velocity_spectrum(self):
+
+        u, v = self.velocity_components()
+        u = u.squeeze()
+        v = v.squeeze()
+
+        th = (self.ds.time - self.ds.time[0]) / np.timedelta64(1, 'h')  # hours since start
+        th = th[np.isfinite(u)]
+        timestep_h = (th[1] - th[0]).values
+        u = u[np.isfinite(u)]
+        v = v[np.isfinite(v)]
+
+        sp = np.abs(np.fft.rfft(np.abs(u + 1j*v)))
+        freq = np.fft.rfftfreq(n=u.size, d=timestep_h)
+
+        da = xr.DataArray(
+            data=sp, name='velocity spectrum',
+            dims=['period'],
+            coords={'period': (['period'], 1/freq, {'units': 'cycles per hour'})},
+            attrs={'units': 'power'}
+            )
+
+        return da
