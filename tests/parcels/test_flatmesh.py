fb624f6de1b3cfc33f1ce25e22953ed55ff827ae
Gaute Hope <gaute.hope@met.no>
2023-03-16 13:56:22 +0100

    parcels: xfail animate

---
+++ tests/parcels/test_flatmesh.py
@@ -0,0 +1,24 @@
+import trajan as _
+import xarray as xr
+import matplotlib.pyplot as plt
+import pytest
+
+
+def test_parcels_flatmesh(plot):
+    ds = xr.open_dataset('tests/test_data/parcels.zarr', engine='zarr')
+    ds = ds.traj.set_crs(None)
+    print(ds)
+    ds.traj.plot()
+
+    if plot:
+        plt.show()
+
+@pytest.mark.xfail
+def test_parcels_flatmesh_animate(plot):
+    ds = xr.open_dataset('tests/test_data/parcels.zarr', engine='zarr')
+    ds = ds.traj.set_crs(None)
+    print(ds)
+    ds.traj.animate()
+
+    if plot:
+        plt.show()
