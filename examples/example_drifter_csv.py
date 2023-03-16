b3fb5b4eec3cf00d81d2af55997981115ecb08b7
Gaute Hope <gaute.hope@met.no>
2023-03-16 12:56:57 +0100

    examples: plot drifters from CSV files

---
+++ examples/example_drifter_csv.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import xarray as xr
+import trajan as _
+import matplotlib.pyplot as plt
+
+#%%
+# Read a CSV file with positions using Pandas.
+ds = pd.read_csv('bug05_pos.csv.xz', parse_dates=['Time'])
+
+#%%
+# Make a synthetic drifter placed slightly east of the original drifter, and concatenate it to the
+# original dataset.
+ds2 = ds.copy()
+ds2['Device'] = 'dev_synthetic'
+ds2['Longitude'] = ds2['Longitude'] + 0.001
+ds = pd.concat((ds, ds2))
+
+#%%
+# Rename the index to `obs`, and rename the other columns to the standard coordinate names.
+ds.index.names = ['obs']
+ds = ds.rename(columns={'Latitude': 'lat', 'Longitude': 'lon', 'Time': 'time', 'Device': 'drifter_names'})
+ds.time = ds['time'].dt.tz_convert(None)
+
+#%%
+# Classify trajectories based on drifter_names.
+ds['trajectory'] = ds.groupby('drifter_names').ngroup()
+ds = ds.set_index(['trajectory', ds.index])
+
+#%%
+# Convert the Pandas DataFrame to Xarray Dataset
+ds = ds.to_xarray()
+
+#%%
+# Simplify the drifter_names variable: It is only dependent on the trajectory dimension.
+#
+# We select the name from the first observation from each trajectory (and drop the 'obs' dimension).
+ds['drifter_names'] = ds.drifter_names.isel(obs=0)
+
+#%%
+# Print the dataset and plot it. See the `example_drifters` example for how to continue analyzing a drifter dataset.
+#
+# We plot with `color=None` to use normal matplotlib color cycling and not a single color for all drifters. This may be changed to be the default in the furture.
+print(ds)
+
+ds.traj.plot(color=None, label=ds.drifter_names.values)
+plt.legend()
+plt.show()
