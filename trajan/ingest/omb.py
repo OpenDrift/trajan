5c8642ee19728050dd9048a18510ea8c0650e411
JRT <jean.rblt@gmail.com>
2023-04-12 14:18:58 +0200

    Draft code structure

---
+++ trajan/ingest/omb.py
@@ -0,0 +1,9 @@
+"""Utilities to import an OMB raw CSV file into Trajan"""
+
+from pathlib import Path
+import trajan as ta
+
+
+def read_omb_csv(path_in: Path):
+    """todo"""
+    print("read_omb")
