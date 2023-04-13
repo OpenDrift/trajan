09d123712a4e1c15a1894a58b8361f42204151b4
JRT <jean.rblt@gmail.com>
2023-04-13 17:03:39 +0200

    Fix paths in tests

--- tests/test_read_omb_csv.py
+++ tests/test_read_omb_csv.py
@@ -1,18 +1,14 @@
-import logging
 from pathlib import Path
 from trajan.readers.omb import read_omb_csv
-import xarray as xr
-import trajan as ta
-import matplotlib.pyplot as plt
 
 
-def test_read_csv_omb_default_waves():
-    path_to_test_data = Path.cwd().parent / "tests" / "test_data" / "csv" / "omb1.csv"
+def test_read_csv_omb_default_waves(test_data):
+    path_to_test_data = test_data / 'csv/omb1.csv'
     xr_result = read_omb_csv(path_to_test_data)
 
 
-def test_read_csv_omb_modified_waves():
-    path_to_test_data = Path.cwd().parent / "tests" / "test_data" / "csv" / "omb2.csv"
+def test_read_csv_omb_modified_waves(test_data):
+    path_to_test_data = test_data / 'csv/omb2.csv'
 
     dict_wave_packet_params = {
         "_BD_YWAVE_PACKET_MIN_BIN": 9,
