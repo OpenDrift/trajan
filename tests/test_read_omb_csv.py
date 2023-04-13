5a0230c4eebee19074b4866323d203fb18cf063b
JRT <jean.rblt@gmail.com>
2023-04-13 16:11:06 +0200

    Fix paths in tests

--- tests/test_read_omb_csv.py
+++ tests/test_read_omb_csv.py
@@ -7,12 +7,12 @@ import matplotlib.pyplot as plt
 
 
 def test_read_csv_omb_default_waves():
-    path_to_test_data = Path.cwd() / "test_data" / "csv" / "omb1.csv"
+    path_to_test_data = Path.cwd().parent / "tests" / "test_data" / "csv" / "omb1.csv"
     xr_result = read_omb_csv(path_to_test_data)
 
 
 def test_read_csv_omb_modified_waves():
-    path_to_test_data = Path.cwd() / "test_data" / "csv" / "omb2.csv"
+    path_to_test_data = Path.cwd().parent / "tests" / "test_data" / "csv" / "omb2.csv"
 
     dict_wave_packet_params = {
         "_BD_YWAVE_PACKET_MIN_BIN": 9,
