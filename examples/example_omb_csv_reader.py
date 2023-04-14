bde325c2d78f35ba906f85e9c16a7991ceac59ad
JRT <jean.rblt@gmail.com>
2023-04-14 10:21:29 +0200

    Simplify example

--- examples/example_omb_csv_reader.py
+++ examples/example_omb_csv_reader.py
@@ -1,51 +1,53 @@
-import logging
+"""
+Reading an OMB Rock7 CSV file into trajan
+============================
+"""
+
 from pathlib import Path
 from trajan.readers.omb import read_omb_csv
-# import xarray as xr
-# import trajan as ta
-# import matplotlib.pyplot as plt
+import trajan as ta
+import coloredlogs
 
-if __name__ == "__main__":
-    # logging.basicConfig(level=logging.DEBUG)
-    logging.basicConfig(level=logging.ERROR)
+# adjust the level of information printed
+# coloredlogs.install(level='error')
+coloredlogs.install(level='debug')
 
-    ####################
-    # example 1: default
-    if True:
-        path_to_test_data = Path.cwd().parent / "tests" / "test_data" / "csv" / "omb1.csv"
+#%%
+# example 1: default configuration of the wave packets
 
-        # for most users, using the default spectra packet format and this will be enough
-        xr_result = read_omb_csv(path_to_test_data)
-        xr_result = xr_result.assign_attrs(
-            {
-                "creator_name": "your name",
-                "creator_email": "your email",
-                "title": "a descriptive title",
-                "summary": "a descriptive summary",
-                "anything_else": "corresponding data",
-            }
-        )
+path_to_test_data = Path.cwd().parent / "tests" / "test_data" / "csv" / "omb1.csv"
 
-        # print(xr_result)
-        # xr_result.to_netcdf("test.nc")
+# for most users, using the default spectra packet format and this will be enough
+xr_result = read_omb_csv(path_to_test_data)
 
-        # ds = xr.open_dataset('test.nc')
-        # ds.traj.plot(margin=1, land='mask', color=None, label=ds.drifter_names.values)
-        # plt.show()
+# remember to add the CF required metadata for your specific deployment
+xr_result = xr_result.assign_attrs(
+    {
+        "creator_name": "your name",
+        "creator_email": "your email",
+        "title": "a descriptive title",
+        "summary": "a descriptive summary",
+        "anything_else": "corresponding data",
+    }
+)
 
-    ####################
-    # example 2: custom size of wave packets; for users who have changed the firmware to transmit
-    # more or less spectrum bins
-    if True:
-        path_to_test_data = Path.cwd().parent / "tests" / "test_data" / "csv" / "omb2.csv"
+# look at the dataset obtained
+print(xr_result)
 
-        dict_wave_packet_params = {
-            "_BD_YWAVE_PACKET_MIN_BIN": 9,
-            "_BD_YWAVE_PACKET_MAX_BIN": 94,
-            "LENGTH_FROM_SERIAL_OUTPUT": 198,
-        }
+#%%
+# example 2: custom size of wave packets; for users who have changed the firmware to transmit
+# more or less spectrum bins
 
-        # if necessary, the user can specify the binary wave packet specification, if this is a modified firmware
-        xr_result = read_omb_csv(path_to_test_data, modified_wave_packet_properties=dict_wave_packet_params)
+path_to_test_data = Path.cwd().parent / "tests" / "test_data" / "csv" / "omb2.csv"
 
-        # print(xr_result)
+# the properties description of the modified binary wave packets
+dict_wave_packet_params = {
+    "_BD_YWAVE_PACKET_MIN_BIN": 9,
+    "_BD_YWAVE_PACKET_MAX_BIN": 94,
+    "LENGTH_FROM_SERIAL_OUTPUT": 198,
+}
+
+# specify the binary wave packet specification corresponding to the modified firmware
+xr_result = read_omb_csv(path_to_test_data, modified_wave_packet_properties=dict_wave_packet_params)
+
+# from there on, all is similar to above
