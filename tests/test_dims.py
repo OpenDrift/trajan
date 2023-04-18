3ab8387009363a19cfb28e0b4bdc0e659a8f846c
Gaute Hope <gaute.hope@met.no>
2023-04-18 21:26:21 +0200

    dims: do not require a time variable

---
+++ tests/test_dims.py
@@ -0,0 +1,14 @@
+from numpy.testing import assert_almost_equal
+import numpy as np
+import trajan as ta
+import xarray as xr
+import pandas as pd
+
+def test_no_time_dim(barents):
+    b = barents.drop_vars('drifter_names')
+    b = b.isel(trajectory=0).mean(dim='obs')
+    assert 'time' not in b.variables
+
+    # can we use trajan without
+
+    print(b.traj)
