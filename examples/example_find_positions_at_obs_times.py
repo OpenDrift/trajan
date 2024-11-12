10295f0665c02af1ceee750e1c202338f8e19f2f
Gaute Hope <gauteh@met.no>
2024-11-12 10:19:08 +0100

    example: add example for interpolating to IMU observations

---
+++ examples/example_find_positions_at_obs_times.py
@@ -0,0 +1,43 @@
+"""
+Finding the closeset or interpolated positions at given times (e.g. wave observations).
+=======================================================================================
+"""
+
+from pathlib import Path
+from trajan.readers.omb import read_omb_csv
+import xarray as xr
+import coloredlogs
+
+coloredlogs.install(level='debug')
+
+#%%
+# Read the data
+data = Path.cwd().parent / "tests" / "test_data" / "csv" / "omb3.csv"
+ds = read_omb_csv(data)
+print(ds)
+
+#%%
+# The wave data in the variable `pHs0` is given along a different observation dimension.
+# Because it is also a observation, i.e. in the style of 2D trajectory
+# datasets, we need to iterate over the trajectories:
+
+
+def gridwaves(tds):
+    t = tds[['lat', 'lon',
+             'time']].traj.gridtime(tds['time_waves_imu'].squeeze())
+    return t.traj.to_2d(obsdim='obs_waves_imu')
+
+
+dsw = ds.groupby('trajectory').map(gridwaves)
+
+print(dsw)
+
+#%%
+# We now have the positions interpolated to the IMU (wave) observations. We
+# could also merge these together to one dataset again:
+
+ds = xr.merge((ds, dsw.rename({
+    'lon': 'lon_waves',
+    'lat': 'lat_waves'
+}).drop('time')))
+print(ds)
